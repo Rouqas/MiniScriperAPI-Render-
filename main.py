e9390fd196898d65e78b1781ae4e0eb81f812e8b
Newton Nath <n.nath@se22.qmul.ac.uk>
2025-02-27 16:18:24 +0000

    chrome path updated for linux environment

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ async def scrape_profile(request: ScrapeRequest):
         options.add_argument("--disable-dev-shm-usage")
 
         # Specify Chrome binary location (Adjust the path for Windows)
-        chrome_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+        chrome_path = "/usr/bin/google-chrome"
         options.binary_location = chrome_path
 
         # Initialize ChromeDriver using WebDriver Manager
